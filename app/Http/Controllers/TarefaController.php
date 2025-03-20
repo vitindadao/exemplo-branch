4f004a2467b1cae84c9e9e51755442a728a89ed6
vitindadao <victor.dadao@hotmail.com>
2025-03-20 09:21:59 -0300

    adiciona migration, model e controller da Tarefa

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    public function index(){
+
+    }
+    
+    public function store(Request $request){
+
+    }
+
+    public function show(string $id){
+
+    }
+
+    public function update(Request $request, string $id){
+
+    }
+
+    public function delete (string $id){
+
+    }
+}
